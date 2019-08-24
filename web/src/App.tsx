ed135df9a0b1ea6e6efaffe11593601f4424fe23
wslyvh <wslyvh@users.noreply.github.com>
2019-08-24 12:31:39 +0200

    dao list

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,32 +1,34 @@
 import { Breadcrumb, Layout, Menu } from "antd";
 import React from "react";
-import "./App.css";
-import { Provider } from 'react-redux'
-import { store } from './backbone/store'
+import { Provider } from "react-redux";
 import AccountComponent from "./AccountComponent/AccountComponent";
+import DaoList from "./DaoListComponent/DaoList";
+import "./App.css";
+import { store } from "./backbone/store";
 import DaoListLoader from "./DaoListComponent/DaoListLoader";
 
 const { Header, Content, Footer } = Layout;
 
 const App: React.FC = () => {
-   return <Provider store={store}>
-       <AccountComponent>
-            <Layout>
-            <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-              <div className="logo">My DAO Dashboard</div>
-            </Header>
-            <Content style={{ padding: "0 50px", marginTop: 64 }}>
-              <div style={{ background: "#fff", padding: 24, minHeight: 380 }}>
-                <p>My DAO Dashboard</p>
-              </div>
-                <DaoListLoader>
-                    DAOS
-                </DaoListLoader>
-            </Content>
-            <Footer style={{ textAlign: "center" }}>ETH Berlin Zwei</Footer>
-            </Layout>
-       </AccountComponent>
+  return (
+    <Provider store={store}>
+      <AccountComponent>
+        <Layout>
+          <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+            <div className="logo">My DAO Dashboard</div>
+          </Header>
+          <Content style={{ padding: "0 50px", marginTop: 64 }}>
+            <div style={{ background: "#fff", padding: 24, minHeight: 380 }}>
+              <p>My DAO Dashboard</p>
+            </div>
+            <DaoListLoader>DAOS</DaoListLoader>
+            <DaoList />
+          </Content>
+          <Footer style={{ textAlign: "center" }}>ETH Berlin Zwei</Footer>
+        </Layout>
+      </AccountComponent>
     </Provider>
+  );
 };
 
 export default App;
