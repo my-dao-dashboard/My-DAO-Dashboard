f49f1bec7ce07331b053ab27ee00276f0dba30f5
wslyvh <wslyvh@users.noreply.github.com>
2019-08-24 23:13:12 +0200

    overview

--- web/src/Components/DaoOverviewComponent/DaoOverview.tsx
+++ web/src/Components/DaoOverviewComponent/DaoOverview.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import { DaoInstanceState, State } from "../../backbone/State";
+import ProposalListComponent from "../ProposalListComponent/ProposalListComponent";
+import { Tag, Row, Col } from "antd";
 
 interface StateProps {
   dao: DaoInstanceState;
@@ -10,7 +12,23 @@ export class DaoOverview extends React.Component<StateProps> {
   public render() {
     return (
       <div>
-        <h2>Participating DAO's</h2>
+        <Row>
+          <Col span={12}>
+            <h2>{this.props.dao.name}</h2>
+          </Col>
+          <Col span={12} style={{ textAlign: "right" }}>
+            <Tag color="cyan">{this.props.dao.kind}</Tag>
+          </Col>
+        </Row>
+        <a href={`https://mainnet.aragon.org/#/${this.props.dao.address}`} target="_blank" rel="noopener noreferrer">
+          <small>{this.props.dao.address}</small>
+        </a>
+        <p>
+          Voting power: {this.props.dao.shareBalance.toNumber()} / {this.props.dao.totalSupply.toString()}
+        </p>
+        <div>
+          <ProposalListComponent />
+        </div>
       </div>
     );
   }
