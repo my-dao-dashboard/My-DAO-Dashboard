faa918a1c391b62ba4fc434811d6dd12752411d6
Sergey Ukustov <sergey.ukustov@machinomy.com>
2019-08-24 16:29:57 +0200

    refactor services

---
+++ web/src/backbone/account.service.ts
@@ -0,0 +1,13 @@
+import Web3 from "web3";
+
+export class AccountService {
+    web3 (): Web3 {
+        const provider = (window as any).web3.currentProvider
+        return new Web3(provider)
+    }
+
+    async getAddress (): Promise<string> {
+        const addresses = await this.web3().eth.getAccounts()
+        return addresses[0]
+    }
+}
