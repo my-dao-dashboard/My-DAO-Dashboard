48d595c2e3107beeb5816ca224891de902bbc8df
Sergey Ukustov <sergey.ukustov@machinomy.com>
2019-08-25 06:41:57 +0200

    Currency qualifier

--- web/src/Components/DaoListComponent/DaoListComponent.tsx
+++ web/src/Components/DaoListComponent/DaoListComponent.tsx
@@ -38,8 +38,8 @@ export class DaoListComponent extends React.Component<StateProps> {
                         <>
                         <p><DaoLink type={item.kind} address={item.address} /></p>
                         <div style={{ maxWidth: "60%"}}>
-                          <Statistic title="Balance" value={item.usdBalance} precision={2} />
-                          <small>Voting power</small>
+                          <Statistic title="Balance" value={item.usdBalance} precision={2} prefix={'$'} />
+                          <small>Voting power: {item.shareBalance} of {item.totalSupply}</small>
                           <Progress percent={
                               Math.round((item.shareBalance / item.totalSupply) * 100)} status="active" />
                         </div>
