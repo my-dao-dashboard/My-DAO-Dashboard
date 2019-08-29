85433a6f787377c5521c649b6e20aff7221e419f
wslyvh <wslyvh@users.noreply.github.com>
2019-08-29 18:59:46 +0200

    account

--- web/src/backbone/account.service.ts
+++ web/src/backbone/account.service.ts
@@ -2,11 +2,11 @@ import Web3 from "web3";
 
 export class AccountService {
   public web3(): Web3 {
-    if (typeof (window as any).ethereum !== "undefined" || typeof (window as any).web3 !== "undefined") {
-      return new Web3((window as any).ethereum || (window as any).web3.currentProvider);
+    if ((window as any).web3) {
+      return new Web3((window as any).web3.currentProvider);
     } else {
       console.log("Web3 not detected. Please login first");
-      return new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/1704f40805834d76b667f4228594b051"));
+      return new Web3();
     }
   }
 
