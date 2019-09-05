ede82351b289ac7adc34fe6481320f0a6bf94a0a
wslyvh <wslyvh@users.noreply.github.com>
2019-09-05 16:23:17 +0200

    account

--- web/src/backbone/account.service.ts
+++ web/src/backbone/account.service.ts
@@ -2,12 +2,8 @@ import Web3 from "web3";
 
 export class AccountService {
   public web3(): Web3 {
-    if ((window as any).web3) {
-      return new Web3((window as any).web3.currentProvider);
-    } else {
-      console.log("Web3 not detected. Please login first");
-      return new Web3();
-    }
+    const provider = (window as any).web3.currentProvider;
+    return new Web3(provider);
   }
 
   public async getAddress(): Promise<string> {
