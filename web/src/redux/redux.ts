0d7d3e76841325a8b6b55a13d146d529de205837
Sergey Ukustov <sergey.ukustov@machinomy.com>
2019-09-06 13:20:51 +0400

    Remove account.redux.ts

--- web/src/redux/redux.ts
+++ web/src/redux/redux.ts
@@ -1,18 +1,15 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import thunk from "redux-thunk";
 import logger from "redux-logger";
-import * as account from "./account.redux";
 import * as settings from "./settings.redux";
 
 export interface State {
-  account: account.AccountState;
   settings: settings.SettingsState;
 }
 
 export const store = createStore(
   combineReducers({
     settings: settings.reducers,
-    account: account.reducers
   }),
   applyMiddleware(thunk, logger)
 );
