aa38126f58a1ffcf180aacc489bbd4950ee7d4e6
Sergey Ukustov <sergey.ukustov@machinomy.com>
2019-09-06 13:17:40 +0400

    Cleanup account.redux

--- web/src/redux/account.redux.ts
+++ web/src/redux/account.redux.ts
@@ -1,16 +1,10 @@
-import actionCreatorFactory from "typescript-fsa";
-import asyncFactory from "typescript-fsa-redux-thunk";
 import { reducerWithInitialState } from "typescript-fsa-reducers";
-import { State } from "./redux";
 import { MetamaskService } from "../services/metamask.service";
 
 export interface AccountState {
   isMetamask: boolean;
   address: string;
 }
-
-const action = actionCreatorFactory("ACCOUNT");
-const asyncAction = asyncFactory<AccountState>(action);
 
 export function initialState(upstream: any): AccountState {
   const isMetamask = Boolean(upstream);
@@ -22,16 +16,4 @@ export function initialState(upstream: any): AccountState {
   };
 }
 
-export const setAddress = action<string>("SET_ADDRESS");
-
-export const enable = asyncAction<void, void>("ENABLE", async (_, dispatch, getState: any) => {
-});
-
-export const reducers = reducerWithInitialState(initialState(MetamaskService.upstreamProvider()))
-  .case(setAddress, (state, address) => {
-    return {
-      ...state,
-      address
-    };
-  })
-  .build();
+export const reducers = reducerWithInitialState(initialState(MetamaskService.upstreamProvider())).build();
