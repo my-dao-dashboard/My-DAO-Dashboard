54c4c62b3e8ebf6222560583f75e9602edfae702
Sergey Ukustov <sergey.ukustov@machinomy.com>
2019-09-06 13:36:09 +0400

    Remove redux folder

--- web/src/application.tsx
+++ web/src/application.tsx
@@ -1,13 +1,11 @@
 import { Icon, Layout } from "antd";
 import React from "react";
-import { Provider } from "react-redux";
 import { Redirect, Route, Switch } from "react-router";
 import "./assets/styles/app.css";
 import { AppHeaderComponent } from "./components/Layout/app-header.component";
 import { SettingsPage } from "./pages/settings.page";
 import { DashboardPage } from "./pages/dashboard.page";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./redux/redux";
 import { AvailableMetamaskContainer } from "./components/account/available-metamask.container";
 import { DaoPage } from "./pages/dao.page";
 
@@ -16,36 +14,34 @@ const { Header, Content, Footer } = Layout;
 const Application: React.FC = () => {
   return (
     <BrowserRouter>
-      <Provider store={store}>
-        <AvailableMetamaskContainer>
-          <Layout>
-            <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
-              <AppHeaderComponent />
-            </Header>
-            <Content className="container">
-              <div className="content">
-                <Switch>
-                  <Route exact path="/" component={DashboardPage} />
-                  <Route exact path="/dao/:address" component={DaoPage} />
-                  <Route exact path="/settings" component={SettingsPage} />
-                  <Redirect to="/" />
-                </Switch>
-              </div>
-            </Content>
-            <Footer style={{ textAlign: "center" }}>
-              <a
-                href="https://github.com/wslyvh/My-DAO-Dashboard"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-muted"
-              >
-                <Icon type="github" />
-              </a>{" "}
-              &nbsp; ETHBerlin Zwei
-            </Footer>
-          </Layout>
-        </AvailableMetamaskContainer>
-      </Provider>
+      <AvailableMetamaskContainer>
+        <Layout>
+          <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+            <AppHeaderComponent />
+          </Header>
+          <Content className="container">
+            <div className="content">
+              <Switch>
+                <Route exact path="/" component={DashboardPage} />
+                <Route exact path="/dao/:address" component={DaoPage} />
+                <Route exact path="/settings" component={SettingsPage} />
+                <Redirect to="/" />
+              </Switch>
+            </div>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>
+            <a
+              href="https://github.com/wslyvh/My-DAO-Dashboard"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-muted"
+            >
+              <Icon type="github" />
+            </a>{" "}
+            &nbsp; ETHBerlin Zwei
+          </Footer>
+        </Layout>
+      </AvailableMetamaskContainer>
     </BrowserRouter>
   );
 };
