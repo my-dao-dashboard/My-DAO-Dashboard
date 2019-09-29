7e3b7e6cb617a640c91fdd9971dbde8f6b50b387
Sergey Ukustov <sergey.ukustov@machinomy.com>
2019-09-29 15:18:07 +0400

    :zap: Load Aragon DAOs faster.
    And, avoid treating some 3rd party contracts as Aragon ones.
    
    Fix #1

--- web/src/services/daos/protocols/aragon.service.ts
+++ web/src/services/daos/protocols/aragon.service.ts
@@ -10,11 +10,7 @@ import { DaoType } from "../../../model/dao-type";
 import { Dao } from "../../../model/dao";
 import { IDaoService } from "../dao.service";
 import { EtherscanService } from "../../etherscan.service";
-
-async function hasMethod(web3: Web3, contractAddress: string, signature: string): Promise<boolean> {
-  const code = await web3.eth.getCode(contractAddress);
-  return code.indexOf(signature.slice(2, signature.length)) > 0;
-}
+import _ from "underscore";
 
 export class AragonService implements IDaoService {
   private readonly ensApollo: ApolloClient<unknown>;
@@ -73,69 +69,67 @@ export class AragonService implements IDaoService {
     throw new Error("Method not implemented.");
   }
 
-  public async getDaosByAccount(address: string): Promise<Dao[]> {
-    const tokenContracts = await this.etherscanService.tokenContractsByAccount(address);
-    const aragonKernels = [];
-    for await (const contractAddress of tokenContracts) {
-      const tokenContract = new this.web3.eth.Contract(aragonTokenABI, contractAddress);
-      const signature = tokenContract.methods.controller().encodeABI();
-      const isAragonToken = await hasMethod(this.web3, contractAddress, signature);
-      if (isAragonToken) {
-        try {
-          const controllerAddress = await tokenContract.methods.controller().call();
-          const controller = new this.web3.eth.Contract(aragonTokenControllerABI, controllerAddress);
-          const kernel: string = await controller.methods.kernel().call();
-          const kernelContract = new this.web3.eth.Contract(aragonKernelABI, kernel);
-          const vaultAddress = await kernelContract.methods
-            .apps(
-              "0xd6f028ca0e8edb4a8c9757ca4fdccab25fa1e0317da1188108f7d2dee14902fb",
-              "0x7e852e0fcfce6551c13800f1e7476f982525c2b5277ba14b24339c68416336d1"
-            )
-            .call();
-          const balance = await this.balanceService.balance(vaultAddress);
-          const a = await this.ensApollo.query({
-            query: gql`
-              query {
-                  domains(where: {resolvedAddress: "${kernel.toLowerCase()}"}) {
-                      name
-                      labelhash
-                      parent {
-                          id
-                      }
-                  }
-              }
-          `
-          });
-          const labelHash = a.data.domains[0].labelhash;
-          const parentId = a.data.domains[0].parent.id.toLowerCase();
-          const name = await this.fetchName(parentId, labelHash);
-          const hiveNames = await this.fetchAllNames();
-          const hiveEntity = hiveNames.find((t: any) => t.address.toLowerCase() === kernel.toLowerCase());
-          const hiveName = hiveEntity ? hiveEntity.name : null;
-          const graphqlName = name ? `${name}.aragonid.eth` : null;
+  public async daoByToken(tokenAddress: string, account: string): Promise<Dao | undefined> {
+    try {
+      const tokenContract = new this.web3.eth.Contract(aragonTokenABI, tokenAddress);
+      const controllerAddress = await tokenContract.methods.controller().call();
+      const controller = new this.web3.eth.Contract(aragonTokenControllerABI, controllerAddress);
+      const kernel: string = await controller.methods.kernel().call();
+      const kernelContract = new this.web3.eth.Contract(aragonKernelABI, kernel);
+      const vaultAddress = await kernelContract.methods
+        .apps(
+          "0xd6f028ca0e8edb4a8c9757ca4fdccab25fa1e0317da1188108f7d2dee14902fb",
+          "0x7e852e0fcfce6551c13800f1e7476f982525c2b5277ba14b24339c68416336d1"
+        )
+        .call();
+      const balance = await this.balanceService.balance(vaultAddress);
+      const a = await this.ensApollo.query({
+        query: gql`
+            query {
+                domains(where: {resolvedAddress: "${kernel.toLowerCase()}"}) {
+                    name
+                    labelhash
+                    parent {
+                        id
+                    }
+                }
+            }
+        `
+      });
+      const labelHash = a.data.domains[0].labelhash;
+      const parentId = a.data.domains[0].parent.id.toLowerCase();
+      const name = await this.fetchName(parentId, labelHash);
+      const hiveNames = await this.fetchAllNames();
+      const hiveEntity = hiveNames.find((t: any) => t.address.toLowerCase() === kernel.toLowerCase());
+      const hiveName = hiveEntity ? hiveEntity.name : null;
+      const graphqlName = name ? `${name}.aragonid.eth` : null;
 
-          const decimals = Number(await tokenContract.methods.decimals().call());
-          const shareBalance = new BigNumber(await tokenContract.methods.balanceOf(address).call())
-            .dividedBy(10 ** decimals)
-            .toNumber();
-          const totalSupply = new BigNumber(await tokenContract.methods.totalSupply().call())
-            .dividedBy(10 ** decimals)
-            .toNumber();
-          const dao: Dao = {
-            address: kernel.toLowerCase(),
-            name: graphqlName || hiveName || kernel,
-            kind: DaoType.ARAGON,
-            shareBalance,
-            totalSupply,
-            balance,
-            usdBalance: balance.reduce((acc, cur) => acc + cur.usdValue, 0)
-          };
-          aragonKernels.push(dao);
-        } catch (ex) {
-          console.log("Failed to parse token: " + contractAddress);
-        }
-      }
+      const decimals = Number(await tokenContract.methods.decimals().call());
+      const shareBalance = new BigNumber(await tokenContract.methods.balanceOf(account).call())
+        .dividedBy(10 ** decimals)
+        .toNumber();
+      const totalSupply = new BigNumber(await tokenContract.methods.totalSupply().call())
+        .dividedBy(10 ** decimals)
+        .toNumber();
+      const dao: Dao = {
+        address: kernel.toLowerCase(),
+        name: graphqlName || hiveName || kernel,
+        kind: DaoType.ARAGON,
+        shareBalance,
+        totalSupply,
+        balance,
+        usdBalance: balance.reduce((acc, cur) => acc + cur.usdValue, 0)
+      };
+      return dao;
+    } catch (ex) {
+      console.warn(`Token is not of Aragon DAO, tokenAddress=${tokenAddress}`);
+      return undefined;
     }
-    return aragonKernels;
+  }
+
+  public async getDaosByAccount(account: string): Promise<Dao[]> {
+    const tokenContracts = await this.etherscanService.tokenContractsByAccount(account);
+    const daos = await Promise.all(tokenContracts.map(tokenAddress => this.daoByToken(tokenAddress, account)));
+    return _.compact(daos);
   }
 }
