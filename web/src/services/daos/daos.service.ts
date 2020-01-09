f6a53e767c27801d8dc37a469fa3c2d9d3276475
Sergey Ukustov <sergey.ukustov@machinomy.com>
2020-01-09 10:41:40 +0400

    Do not load daostack

--- web/src/services/daos/daos.service.ts
+++ web/src/services/daos/daos.service.ts
@@ -5,7 +5,7 @@ import Web3 from "web3";
 import { first, flatMap, map } from "rxjs/operators";
 import { MolochService } from "./protocols/moloch.service";
 import { AragonService } from "./protocols/aragon.service";
-import { DaostackService } from "./protocols/daostack.service";
+// import { DaostackService } from "./protocols/daostack.service";
 import { BalanceService } from "../balance.service";
 import { Dao } from "../../model/dao";
 import { EtherscanService } from "../etherscan.service";
@@ -17,7 +17,7 @@ export class DaosService {
   private readonly balanceService$: Observable<BalanceService>;
   private readonly molochService$: Observable<MolochService>;
   private readonly aragonService$: Observable<AragonService>;
-  private readonly daostackService$: Observable<DaostackService>;
+  // private readonly daostackService$: Observable<DaostackService>;
 
   constructor(
     watchedAddresses$: Observable<string[]>,
@@ -36,7 +36,7 @@ export class DaosService {
     this.aragonService$ = zip(web3$, this.balanceService$).pipe(
       map(p => new AragonService(p[0], p[1], etherscanService))
     );
-    this.daostackService$ = this.balanceService$.pipe(map(bs => new DaostackService(bs)));
+    // this.daostackService$ = this.balanceService$.pipe(map(bs => new DaostackService(bs)));
 
     zip(watchedAddresses$, account$)
       .pipe(map(p => p[0].concat(p[1])))
@@ -57,8 +57,8 @@ export class DaosService {
   async allByAccount(address: string): Promise<Dao[]> {
     const molochDaos$ = this.molochService$.pipe(flatMap(s => s.getDaosByAccount(address)));
     const aragonDaos$ = this.aragonService$.pipe(flatMap(s => s.getDaosByAccount(address)));
-    const daostackDaos$ = this.daostackService$.pipe(flatMap(s => s.getDaosByAccount(address)));
-    const daos = zip(molochDaos$, aragonDaos$, daostackDaos$).pipe(
+    // const daostackDaos$ = this.daostackService$.pipe(flatMap(s => s.getDaosByAccount(address)));
+    const daos = zip(molochDaos$, aragonDaos$).pipe(
       map(d => d.flat()),
       first()
     );
